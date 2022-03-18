8e3d22117fa1630ce1661558b5cb983b517236ce
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2022-03-18 15:10:19 +0100

    BCrypt password encoder

--- src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
+++ src/test/java/guru/sfg/brewery/web/controllers/PasswordEncodingTest.java
@@ -1,9 +1,11 @@
 package guru.sfg.brewery.web.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.LdapShaPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.password.StandardPasswordEncoder;
 import org.springframework.util.DigestUtils;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -11,6 +13,24 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class PasswordEncodingTest {
 
     final static String PASSWORD = "password";
+
+    @Test
+    void testBCrypt() {
+        PasswordEncoder bcrypt = new BCryptPasswordEncoder(13);
+
+        // different values will be displayed as a random salt is being used
+        System.out.println(bcrypt.encode(PASSWORD));
+        System.out.println(bcrypt.encode(PASSWORD));
+    }
+
+    @Test
+    void testSha256() {
+        PasswordEncoder sha256 = new StandardPasswordEncoder();
+
+        // different values will be displayed as a random salt is being used
+        System.out.println(sha256.encode(PASSWORD));
+        System.out.println(sha256.encode(PASSWORD));
+    }
 
     @Test
     void testLdap() {
