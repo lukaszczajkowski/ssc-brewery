b40663f4abc276cbd77dcbd360233ca8c067dba8
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2022-03-18 13:28:23 +0100

    HTTP method matching

---
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
@@ -0,0 +1,23 @@
+package guru.sfg.brewery.web.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
+public class BeerRestControllerIT extends BaseIT {
+
+    @Test
+    void findBeers() throws Exception{
+        mockMvc.perform(get("/api/v1/beer"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findBeerById() throws Exception {
+        mockMvc.perform(get("/api/v1/beer/5fb8b490-2156-4b0f-9209-e6a748956f87"))
+                .andExpect(status().isOk());
+    }
+}
