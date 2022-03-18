c773c71eec5e542615ead52c711a37edd08c351b
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2022-03-18 13:09:27 +0100

    Permit all with url pattern matching

---
+++ src/test/java/guru/sfg/brewery/web/controllers/BaseIT.java
@@ -0,0 +1,48 @@
+package guru.sfg.brewery.web.controllers;
+
+import guru.sfg.brewery.repositories.BeerInventoryRepository;
+import guru.sfg.brewery.repositories.BeerRepository;
+import guru.sfg.brewery.repositories.CustomerRepository;
+import guru.sfg.brewery.services.BeerService;
+import guru.sfg.brewery.services.BreweryService;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+public class BaseIT {
+    //this brings the Spring Security filters
+    @Autowired
+    WebApplicationContext context;
+
+    MockMvc mockMvc;
+
+    @MockBean
+    BeerRepository beerRepository;
+
+    @MockBean
+    BreweryService breweryService;
+
+    @MockBean
+    BeerInventoryRepository beerInventoryRepository;
+
+    @MockBean
+    CustomerRepository customerRepository;
+
+    @MockBean
+    BeerService beerService;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders
+                // web app context needs to be injected here
+                .webAppContextSetup(context)
+                // this activates spring security
+                .apply(springSecurity())
+                .build();
+    }
+}
