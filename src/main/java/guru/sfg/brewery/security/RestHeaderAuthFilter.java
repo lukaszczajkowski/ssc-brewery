1ff8a1097abd688de39ee5a689ff41700901a8f0
lukaszczajkowski <lukaszczajkowski.wroc@gmail.com>
2022-03-21 10:14:19 +0100

    Custom authentication filter

---
+++ src/main/java/guru/sfg/brewery/security/RestHeaderAuthFilter.java
@@ -0,0 +1,106 @@
+package guru.sfg.brewery.security;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+import org.springframework.util.StringUtils;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.apache.logging.log4j.util.Strings.isBlank;
+
+@Slf4j
+public class RestHeaderAuthFilter extends AbstractAuthenticationProcessingFilter {
+
+    public RestHeaderAuthFilter(RequestMatcher requiresAuthenticationRequestMatcher) {
+        super(requiresAuthenticationRequestMatcher);
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) req;
+        HttpServletResponse response = (HttpServletResponse) res;
+
+        if (this.logger.isDebugEnabled()) {
+            this.logger.debug("Request is to process authentication");
+        }
+
+        try {
+            Authentication authResult = attemptAuthentication(request, response);
+
+            if (authResult != null) {
+                successfulAuthentication(request, response, chain, authResult);
+            } else {
+                chain.doFilter(request, response);
+            }
+        } catch (AuthenticationException e) {
+            unsuccessfulAuthentication(request, response, e);
+        }
+    }
+
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        SecurityContextHolder.clearContext();
+
+        if (log.isDebugEnabled()) {
+            log.debug("Authentication request failed: " + failed.toString(), failed);
+            log.debug("Updated SecurityContextHolder to contain null Authentication");
+        }
+
+        response.sendError(HttpStatus.UNAUTHORIZED.value(),
+                HttpStatus.UNAUTHORIZED.getReasonPhrase());
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws AuthenticationException, IOException, ServletException {
+        String userName = getUserName(httpServletRequest);
+        String password = getPassword(httpServletRequest);
+
+        if (isBlank(userName)) {
+            userName = "";
+        }
+
+        if (isBlank(password)) {
+            password = "";
+        }
+
+        log.debug("Authenticating user: " + userName);
+
+        UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(userName, password);
+
+        if (!StringUtils.isEmpty(userName)) {
+            return getAuthenticationManager().authenticate(token);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
+        if (this.logger.isDebugEnabled()) {
+            this.logger.debug("Authentication success. Updating SecurityContextHolder to contain: " + authResult);
+        }
+
+        SecurityContextHolder.getContext().setAuthentication(authResult);
+    }
+
+
+    private String getPassword(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getHeader("Api-Secret");
+    }
+
+    private String getUserName(HttpServletRequest httpServletRequest) {
+        return httpServletRequest.getHeader("Api-Key");
+    }
+}
--- src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
+++ src/test/java/guru/sfg/brewery/web/controllers/BeerRestControllerIT.java
@@ -3,6 +3,8 @@ package guru.sfg.brewery.web.controllers;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -25,5 +27,34 @@ public class BeerRestControllerIT extends BaseIT {
     void findBeerByUpc() throws Exception {
         mockMvc.perform(get("/api/v1/beerUpc/0631234300019"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteBeer() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/5fb8b490-2156-4b0f-9209-e6a748956f87")
+            .header("Api-Key", "spring")
+            .header("Api-Secret", "guru"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void deleteBeerBadCreds() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/5fb8b490-2156-4b0f-9209-e6a748956f87")
+                .header("Api-Key", "spring")
+                .header("Api-Secret", "guru134"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    void deleteBeerHttpBasic() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/5fb8b490-2156-4b0f-9209-e6a748956f87")
+                .with(httpBasic("spring", "guru")))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+    @Test
+    void deleteBeerNoAuth() throws Exception {
+        mockMvc.perform(delete("/api/v1/beer/5fb8b490-2156-4b0f-9209-e6a748956f87"))
+                .andExpect(status().isUnauthorized());
     }
 }
